790e035470d793205d9ca49cfade8817875ae6ff
undefinedyobra <brianmelly008@gmail.com>
2024-03-22 12:06:41 +0300

    looping code

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, eventName) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return messages;
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
